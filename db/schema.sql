c16f23be0f87b9853d6d9e954c008fa528cf6d5f
Wanda Gleason <oglissane@gmail.com>
2021-03-14 15:57:30 -0400

    Edited typos in id names for different tables.

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE departments (
     dept_name VARCHAR(30) UNIQUE NOT NULL
 );
 CREATE TABLE roles (
-    roles_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    role_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     title VARCHAR(30) UNIQUE NOT NULL,
     salary DECIMAL(10,2),
     dept_id INT NOT NULL FOREIGN KEY
@@ -18,6 +18,6 @@ CREATE TABLE employees (
     name VARCHAR(255) UNIQUE NOT NULL,
     first_name VARCHAR(30) NOT NULL,
     last_name VARCHAR(30) NOT NULL,
-    roles_id INT FOREIGN KEY,
+    role_id INT FOREIGN KEY,
     manager_id INT UNIQUE NOT NULL FOREIGN KEY
 );
