5c6160312be1c4f81cf2c5aafb008b14dc1d8e5e
Wanda Gleason <oglissane@gmail.com>
2021-03-13 20:48:50 -0500

    Edited connection code - now working.

--- server.js
+++ server.js
@@ -1,24 +1,50 @@
-require('dotenv').config()
+require('dotenv').config();
 
 // call once somewhere in the beginning of the app
-const mysql = require("mysql");
+var mysql = require("mysql");
 const inquirer = require("inquirer");
 const dotenv = require('dotenv').config();
 const cTable = require('console.table');
 
-const cTable = require('console.table');
-console.table([
-  {
-    name: 'foo',
-    age: 10
-  }, {
-    name: 'bar',
-    age: 20
-  }
-]);
 
-// prints
-name  age
-----  ---
-foo   10
-bar   20
+
+// Enable access to .env variables
+require('dotenv').config();
+
+// Use environment variables to connect to database
+var connection = new mysql.createConnection({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME
+});
+// port: 3306
+connection.connect((err) => {
+    if (err) {
+        throw err;
+    }
+    console.log("Wow, it's connected!");
+});
+
+
+module.exports = mysql;
+
+
+
+
+
+// console.table([
+//   {
+//     name: 'foo',
+//     age: 10
+//   }, {
+//     name: 'bar',
+//     age: 20
+//   }
+// ]);
+
+// // prints
+// name  age
+// ----  ---
+// foo   10
+// bar   20
