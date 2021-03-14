d4d9698e96d67122ddb206e32c214e438343c2d0
Wanda Gleason <oglissane@gmail.com>
2021-03-13 23:29:49 -0500

    Adding code to create database and tables.

--- db/schema.sql
+++ db/schema.sql
@@ -1,2 +1,23 @@
 DROP DATABASE IF EXISTS employees_db;
 CREATE DATABASE employees_db;
+USE employees_db;
+
+CREATE TABLE departments (
+    dept_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    dept_name VARCHAR(30) UNIQUE NOT NULL
+);
+CREATE TABLE roles (
+    roles_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    title VARCHAR(30) UNIQUE NOT NULL,
+    salary DECIMAL(10,2),
+    dept_id INT NOT NULL FOREIGN KEY
+);
+
+CREATE TABLE employees (
+    employee_id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) UNIQUE NOT NULL,
+    first_name VARCHAR(30) NOT NULL,
+    last_name VARCHAR(30) NOT NULL,
+    roles_id INT FOREIGN KEY,
+    manager_id INT UNIQUE NOT NULL FOREIGN KEY
+);
